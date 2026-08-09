885039a87de7c98be3aadc48453711315a3c436e
private2547 <abbaismailkaugama@gmail.com>
2026-08-09 22:33:05 +0100

    Add click event listener to buttons for alerts

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        alert("Thank you for contacting Abba Ismail Global Services!");
+    });
+});
